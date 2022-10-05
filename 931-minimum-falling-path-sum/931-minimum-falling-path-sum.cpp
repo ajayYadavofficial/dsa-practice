f5a53a1b4e0690be079ef9bd28e9325ef6298f0b
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2022-10-05 15:10:13 +0530

    Time: 321 ms (5.00%), Space: 46.8 MB (5.17%) - LeetHub

---
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    
+    int f(int i,int j, vector<vector<int>>& matrix, int n, vector<vector<int>>& dp){
+        
+//         base case to stop
+        if(i >= n-1) return matrix[n-1][j];
+        
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+//         cases
+//         left down
+        int leftDia = INT_MAX, rightDia = INT_MAX;
+        if(j-1 >= 0){
+             leftDia = matrix[i][j] + f(i+1, j-1, matrix,n,dp);
+        }
+//         right Down
+        if(j+1 < n){
+            rightDia = matrix[i][j] + f(i+1, j+1, matrix,n,dp);
+        }
+        
+//         down
+        int down = matrix[i][j] + f(i+1, j, matrix,n,dp);
+        
+        
+        return dp[i][j] = min(leftDia, min(rightDia,down));
+        
+    }
+    
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int ans = INT_MAX;
+        for(int j= 0;j<n;j++){
+            vector<vector<int>> dp (n, vector<int>(n,-1));
+            /*becoz there are variabel starting point so we have to compute for each stating point in the 1st row therefore we are calling recursion for each element of 1st row*/
+            int currans = f(0,j,matrix,n,dp);
+            if(currans < ans) ans = currans;
+        }
+        return ans;
+    }
+};
