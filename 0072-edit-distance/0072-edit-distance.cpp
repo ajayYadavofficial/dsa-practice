f856c7abfab8a8bc0708629e441dab8bd22234e9
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2022-10-12 19:45:21 +0530

    Time: 37 ms (32.00%), Space: 9 MB (60.13%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -22,9 +22,29 @@ public:
         int n = s1.size();
         int m = s2.size();
         
-        vector<vector<int>> dp (n+1, vector<int>(m+1, -1));
+        // vector<vector<int>> dp (n+1, vector<int>(m+1, -1));
+        vector<vector<int>> dp (n+1, vector<int>(m+1, 0));
         
-        return f(n,m, s1,s2, dp);
+        for(int j = 0; j<=m; j++) dp[0][j] = j; 
+        for(int i = 1; i<=n; i++) dp[i][0] = i;// filling from 1 becoz 0 th case of i is already taken care of
+        
+        for(int i= 1; i<=n;i++){
+            for(int j=1; j<=m; j++){
+                
+                if(s1[i-1] == s2[j-1]) dp[i][j] = 0 + dp[i-1][j-1];
+        
+        //         not Match
+                else{
+                int insert = 1 + dp[i][j-1];
+                int remove = 1 + dp[i-1][j];
+                int replace = 1 + dp[i-1][j-1];
+                dp[i][j] = min(insert, min(remove, replace));
+                }
+                
+            }
+        }
+        
+        return dp[n][m];
         
     }
 };
