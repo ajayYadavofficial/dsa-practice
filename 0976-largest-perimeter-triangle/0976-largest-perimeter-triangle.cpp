e3b0743a12b87dce8b3b952b02fc19be03161575
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2022-10-12 17:39:09 +0530

    Time: 75 ms (40.50%), Space: 21.7 MB (96.06%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
     
-    bool isTriangle(int a,int b, int c){
-        return (a < b+c && b < a+c && c<a+b) ;
-    }
+   int largestPerimeter(vector<int>& nums) {
     
-    int largestPerimeter(vector<int>& nums) {
-        int a,b,c, n = nums.size();
-        int maxPeri = 0;
-        sort(nums.begin(), nums.end());
-        for(int i = n-1; i>=2; i--){
-            a = nums[i];
-            b = nums[i-1];
-            c = nums[i-2];
-            // if(isTriangle(a,b,c)) cout<<"YES"<<endl;
-            if(isTriangle(a,b,c) && a+b+c > maxPeri) {
-                maxPeri = a+b+c;
-            }
+    sort(nums.begin(), nums.end());                 //sort the array
+    
+    for(int i=nums.size()-3; i>=0; i--){            //traverse from n-3 to 0
+        if(nums[i]+nums[i+1] > nums[i+2]) {         //if at any instant a + b > c, return perimenter (a+b+c)
+            return  nums[i]+nums[i+1]+nums[i+2];    // a=nums[i], b=nums[i+1], c=nums[i+2];
         }
-        return maxPeri;
     }
+    return 0;                                       //else return 0
+}
+    
 };
