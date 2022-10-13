44508d87b10757463adffed13dd913ecb4817829
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2022-10-13 19:43:47 +0530

    Time: 167 ms (23.22%), Space: 9.6 MB (74.70%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -33,7 +33,39 @@ public:
     bool isMatch(string s, string p) {
         int n = p.size();
         int m = s.size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
-        return f(n,m,s,p, dp);
+        vector<vector<bool>> dp(n+1, vector<bool>(m+1,false));
+        //base cases
+        dp[0][0] = true;
+        // for(int j=1; j<=m,j++) dp[0][j] = false;
+        for(int i=1; i<=n;i++) {
+            for(int k = 0; k<i;k++){
+                if(p[k] != '*') {
+                    dp[i][0] = false;
+                    break;
+                }
+                else dp[i][0] = true;
+            }
+        }
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                
+                if(p[i-1] == s[j-1] || p[i-1] == '?'){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+
+        //         p[i] == '*'
+                else if(p[i-1] == '*'){
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }
+
+        //         this is the case if the char is dissimilar like e<->f
+                else dp[i][j] = false;
+
+            }
+        }
+    
+        
+        return dp[n][m];
     }
 };
