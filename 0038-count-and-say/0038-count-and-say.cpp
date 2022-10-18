e7db6af92859911694938feade216838ace39713
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2022-10-18 09:07:32 +0530

    Time: 8 ms (78.55%), Space: 7.1 MB (33.04%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    
+    string f(int n, string s){
+        if(n == 0) return s;
+        string ans = "";
+        char ch = '1';
+        for(int i = 0; i<s.size(); i++){
+            if(s[i] == s[i+1]){
+                ch++;
+            }else{
+                ans += ch;
+                ans +=s[i];
+                ch = '1';
+            }
+        }
+        // cout<<ans<<endl;
+        return f(n-1,ans);
+    }
+    
+    string countAndSay(int n) {
+        return f(n-1,"1");  
+    }
+};
