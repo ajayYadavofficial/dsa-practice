bd734f14dfcc0125e3e86f673c2eb59352d5a265
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2022-11-09 09:58:07 +0530

    Time: 303 ms (79.14%), Space: 84.1 MB (98.88%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -0,0 +1,39 @@
+class StockSpanner {
+public:
+    StockSpanner() {
+        
+    }
+    stack<pair<int, int>> st;
+    int next(int price) {
+         int count = 0;
+
+        // find no. of elements less than or equal to curr price
+
+        while(st.empty() == false && st.top().first <= price)
+        {
+            // update count
+
+            count += st.top().second;
+
+            // pop the smaller element
+
+            st.pop();
+        }
+
+        // increment the count for curr price
+
+        count++;
+
+        // push the {price, count} into stack
+
+        st.push({price, count});
+
+        return count;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
