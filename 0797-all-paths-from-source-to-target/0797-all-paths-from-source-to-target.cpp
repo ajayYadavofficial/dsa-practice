eb91b202293fbd25222b280bed40c75140be4fab
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2022-12-30 11:35:42 +0530

    Time: 11 ms (93.89%), Space: 10.7 MB (83.12%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -0,0 +1,38 @@
+// class Solution {
+// public:
+//     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+//         vector<vector<int>> paths;
+        
+//         queue<int> q;
+//         q.push(0);
+        
+//         while(!q.empty()){
+            
+//             int node = q.front();
+//             q.pop();
+            
+//             for(auto it : graph[node]){
+                
+//             }
+            
+//         }
+        
+//     }
+// };
+class Solution {
+public:
+    void dfs(vector<vector<int>>& graph, vector<vector<int>>& ans, vector<int>& v, int start) {
+        for (int i = 0; i < graph[start].size(); i++) {
+            v.push_back(graph[start][i]);
+            if (graph[start][i] == graph.size() - 1) ans.push_back(v);
+            dfs(graph, ans, v, graph[start][i]);
+            v.pop_back();
+        }
+    }
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<int> v; v.push_back(0);
+        vector<vector<int>> ans;
+        dfs(graph, ans, v, 0);
+        return ans;
+    }
+};
