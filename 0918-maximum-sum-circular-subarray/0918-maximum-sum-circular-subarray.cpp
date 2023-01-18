9192e2c45fd745199510247aae1d34aca8c0b3b4
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2023-01-18 22:49:59 +0530

    Time: 63 ms (91.29%), Space: 39.9 MB (77.39%) - LeetHub

---
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -0,0 +1,43 @@
+// class Solution {
+// public:
+    
+    
+//     int maxSubarraySumCircular(vector<int>& nums) {
+        
+//         int n = nums.size();
+//         int maxSum = -1e8;
+//         for(int i=0;i<n;i++){
+            
+//             int stIndx = i;
+//             int sum = nums[i];
+//             i = i + 1;
+//             i = i % n;
+//             while(stIndx != i){
+//                 sum += nums[i];
+//                 if(sum > maxSum)
+//                     maxSum = sum;
+//                 i = i+1;
+//                 i = i%n;
+                
+//             }
+//             //if(sum > maxSum ) maxSum = sum;
+//         }
+//         return maxSum;
+//     }
+// };
+
+class Solution {
+public:
+    int maxSubarraySumCircular(vector<int>& A) {
+        int total_sum=0,curr_sum1=0,curr_sum2=0,mxsum_subary=INT_MIN,minsum_subary=INT_MAX;   
+        for(auto i:A)
+        {
+            total_sum+=i; curr_sum1+=i; curr_sum2+=i;
+            mxsum_subary=max(mxsum_subary,curr_sum1);
+            if(curr_sum1<0) curr_sum1=0;
+           minsum_subary=min(curr_sum2,minsum_subary);
+            if(curr_sum2>0) curr_sum2=0;
+        }
+        return (total_sum==minsum_subary)?mxsum_subary:max(mxsum_subary,total_sum-minsum_subary);  
+    }
+};
