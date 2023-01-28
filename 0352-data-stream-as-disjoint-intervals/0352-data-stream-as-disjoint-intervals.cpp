77249c5372e5bd7b9aeb947237f39ef07c245dca
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2023-01-28 12:30:00 +0530

    Time: 150 ms (17.25%), Space: 50.6 MB (5.10%) - LeetHub

--- 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.cpp
+++ 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.cpp
@@ -1,55 +1,29 @@
 class SummaryRanges {
 public:
+    set<int> st;
     SummaryRanges() {
-        
+        st.clear();
     }
     
     void addNum(int value) {
-        auto it = _map.lower_bound(value);
-        bool merged = false;
-        if(it != _map.begin()) {
-            auto prev = it;
-            --prev;
-            if(prev->second + 1 >= value) {
-                merged = true;
-                prev->second = max(prev->second, value);
-            }
-        }
-
-        if(it != _map.end()) {
-            if(it->first - 1 <= value) {
-                if(merged) {
-                    auto prev = it;
-                    --prev;
-                    if(prev->second >= it->first - 1) {
-                        prev->second = max(prev->second, it->second);
-                        _map.erase(it);
-                    }
-                } else {
-                    merged = true;
-                    if(it->first != value) {
-                        pair<int, int> p = *it;
-                        p.first = min(p.first, value);
-                        it = _map.insert(it, p);
-                        ++it;
-                        if(it != _map.end())
-                            _map.erase(it);
-                    }
-                }
-            }
-        }
-        if(!merged)
-            _map.insert(it, {value, value});
+        st.insert(value);
     }
     
     vector<vector<int>> getIntervals() {
-        vector<vector<int>> intervals;
-        for(auto const & p : _map)
-            intervals.push_back({p.first, p.second});
-        return intervals;
+        
+        vector<int> vec(begin(st), end(st));
+        vector<vector<int>> result;
+        int n = vec.size();
+        for(int i=0; i<n;i++){
+            int left = vec[i];
+            while(i < n-1 && vec[i] + 1 == vec[i+1]){
+                i++;
+            }
+            
+            result.push_back({left, vec[i]});
+        }
+        return result;
     }
-
-    map<int, int> _map;
 };
 
 /**
