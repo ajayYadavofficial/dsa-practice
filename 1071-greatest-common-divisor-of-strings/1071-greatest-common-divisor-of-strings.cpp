ae3e6996b2d414dcd3013dc06214f6d5243f3772
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2023-02-01 11:10:28 +0530

    Time: 3 ms (83.39%), Space: 7.3 MB (43.04%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,7 +1,14 @@
 class Solution {
 public:
-    string gcdOfStrings(string str1, string str2) {
-        return (str1 + str2 == str2 + str1)? 
-        str1.substr(0, gcd(size(str1),size(str2))): "";
+    string gcdOfStrings(string s1, string s2) {
+        
+        if( s1 + s2 != s2 + s1) return "";
+        
+        int n = s1.size();
+        int m = s2.size();
+        
+        int cmn = __gcd(n,m);
+        
+        return s1.substr(0,cmn);
     }
 };
