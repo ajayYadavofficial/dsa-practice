566412fbcca5b7989a2d6d7f5bfdd6b37bd9f1d5
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2023-02-06 17:53:31 +0530

    Time: 3 ms (92.72%), Space: 9.9 MB (31.98%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -1,16 +1,29 @@
+// class Solution {
+// public:
+//     vector<int> shuffle(vector<int>& nums, int n) {
+//         vector<int> ans;
+//         int i = 0, j = n;
+//         while( i < n && j < 2*n){
+//             cout<<i<<" "<<j<<endl;
+//             ans.push_back(nums[i]);
+//             ans.push_back(nums[j]);
+//             i++;
+//             j++;
+            
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int> ans;
-        int i = 0, j = n;
-        while( i < n && j < 2*n){
-            cout<<i<<" "<<j<<endl;
-            ans.push_back(nums[i]);
-            ans.push_back(nums[j]);
-            i++;
-            j++;
-            
+        vector<int> res;
+        for(int i=0,j=nums.size()/2;i<nums.size()/2,j<nums.size();i++,j++) {
+            res.push_back(nums[i]);
+            res.push_back(nums[j]);
+
         }
-        return ans;
+        return res;
     }
 };
