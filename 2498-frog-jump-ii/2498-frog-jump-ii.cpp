9e7fb39cb951523dcd0b89f63a0bf0a5e8ee29ba
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2023-03-10 11:36:37 +0530

    Time: 145 ms (83.52%), Space: 65.3 MB (70.03%) - LeetHub

--- 2498-frog-jump-ii/2498-frog-jump-ii.cpp
+++ 2498-frog-jump-ii/2498-frog-jump-ii.cpp
@@ -3,7 +3,7 @@ public:
     int maxJump(vector<int>& stones) {
         int res=stones[1]-stones[0]; // for size 2
         for(int i=0; i<stones.size()-2; i++)
-            res=max(res, max(stones[i+2], stones[i+1])-stones[i]);
+            res=max(res, stones[i+2]-stones[i]);
         return res;
     }
 };
