6fd1b07647ebe81da0ea2186d971c77bfa2be98b
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2023-03-15 22:07:25 +0530

    Time: 30 ms (84.14%), Space: 13.2 MB (84.03%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,13 +1,51 @@
+// class Solution {
+// public:
+//     int kthSmallest(vector<vector<int>>& matrix, int k) {
+//         priority_queue<int> maxHeap;
+//         for(auto it : matrix){
+//             for(auto it1 : it){
+//                 maxHeap.push(it1);
+//                 if(maxHeap.size() > k) maxHeap.pop();
+//             }  
+//         }
+//         return maxHeap.top();   
+//     }
+// };
+
 class Solution {
 public:
-    int kthSmallest(vector<vector<int>>& matrix, int k) {
-        priority_queue<int> maxHeap;
-        for(auto it : matrix){
-            for(auto it1 : it){
-                maxHeap.push(it1);
-                if(maxHeap.size() > k) maxHeap.pop();
-            }  
+    
+    int findRank( int mid, vector<vector<int>> & mat){
+        int n = mat.size();
+        int i = n-1, j = 0;
+        int cnt = 0;
+        while(i >=0 && j < n){
+            if(mat[i][j] > mid) i--;
+            else{
+                cnt += i+1;
+                j++;
+            }
         }
-        return maxHeap.top();   
+        return cnt;
+    }
+    
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n = matrix.size();
+        int lo = matrix[0][0];
+        int hi = matrix[n-1][n-1];
+        int ans = 0;
+        while( lo < hi){
+            int mid = lo  + (hi-lo)/2;
+            
+            int rnk = findRank(mid, matrix);
+            
+            if(rnk < k){
+                lo = mid+1;
+            }
+            else{
+                hi = mid;
+            }
+        }
+        return lo;
     }
 };
