a7a91e1c41690a084a0b78e83bab447a36073458
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2023-03-22 10:21:40 +0530

    Time: 896 ms (8.16%), Space: 211.6 MB (5.24%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int minScore(int n, vector<vector<int>>& roads) {
+        
+        vector<vector<int>> adj[n+1];
+        for(auto it : roads){
+            int u = it[0];
+            int v = it[1];
+            int dist = it[2];
+            adj[u].push_back({v,dist});
+            adj[v].push_back({u,dist});
+        }
+        
+        vector<bool> visited(n+1,0);
+        int ans = INT_MAX;
+        visited[1] = true;
+        queue<int> q; // <node>
+        q.push(1);
+        
+        while(!q.empty()){
+            auto node = q.front();
+            q.pop();
+            
+            for(auto it : adj[node]){
+                // cout<<*it<<endl;
+                int adjNode = it[0];
+                int dist = it[1];
+                
+                if(!visited[adjNode]){
+                    visited[adjNode] = true;
+                    q.push(adjNode);
+                }
+            }
+            
+        }
+        
+        for(auto it : roads){
+            int u = it[0];
+            int v = it[1];
+            int dist = it[2];
+            
+            if(visited[u] && visited[v]){
+                ans = min(ans,dist);
+            }
+            
+        }
+        return ans;
+        
+    }
+};
