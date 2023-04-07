31a508c90d91b470ac303438f11c1c409631df09
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2023-04-07 15:20:56 +0530

    Time: 0 ms (100.00%), Space: 6 MB (59.72%) - LeetHub

--- 2550-count-collisions-of-monkeys-on-a-polygon/2550-count-collisions-of-monkeys-on-a-polygon.cpp
+++ 2550-count-collisions-of-monkeys-on-a-polygon/2550-count-collisions-of-monkeys-on-a-polygon.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
-    int M = 1000000007;
+    int M = 1e9+7;
     long long pow_temp(long long n){
         if(n == 1) return 2;
         long long t = pow_temp(n/2);
         if(n%2 == 0) return (t*t) % M;
-        // at each step we're taking the modulus so that value does'nt exceed the int limit
         return (2 * (t*t)%M)%M;
     }
     
     int monkeyMove(int n) {
-        //we're not directly using the inbuilt power function bcoz value may exceed the int range
+//         total number of ways 2^n - no. of ways where no cillision happend (2)
         int t = pow_temp(n)%M - 2;
         return (t < 0)? M + t: t;
     }
