c03ee7f0360e89781fb6be3591bb76083021b4e6
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2023-04-14 20:59:49 +0530

    Added solution - LeetHub

---
+++ Course Schedule - GFG/course-schedule.cpp
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+  public:
+    vector<int> findOrder(int n, int m, vector<vector<int>> pre) 
+    {
+        //code here
+        // directed graph
+        vector<int> adj[n];
+        vector<int> indegree(n,0);
+        for( auto it : pre){
+            int u = it[1];
+            int v = it[0];
+            adj[u].push_back(v);
+            indegree[v]++;
+        }
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(indegree[i] == 0) q.push(i);
+        }
+        vector<int> topo;
+        while(!q.empty()){
+            int node = q.front();
+            topo.push_back(node);
+            q.pop();
+            for( auto it : adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0) q.push(it);
+            }
+        }
+        if(topo.size() != n) return{};
+        return topo;
+    }
+};
+
+//{ Driver Code Starts.
+
+int check(int V, vector <int> &res, vector<int> adj[]) {
+    vector<int> map(V, -1);
+    for (int i = 0; i < V; i++) {
+        map[res[i]] = i;
+    }
+    for (int i = 0; i < V; i++) {
+        for (int v : adj[i]) {
+            if (map[i] > map[v]) return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int n, m;
+        cin >> n >> m;
+        int u, v;
+
+        vector<vector<int>> prerequisites;
+
+        for (int i = 0; i < m; i++) {
+            cin >> u >> v;
+            prerequisites.push_back({u,v});
+        }
+        
+        vector<int> adj[n];
+        for (auto pre : prerequisites)
+            adj[pre[1]].push_back(pre[0]);
+        
+        Solution obj;
+        vector <int> res = obj.findOrder(n, m, prerequisites);
+        if(!res.size())
+            cout<<"No Ordering Possible"<<endl;
+        else
+            cout << check(n, res, adj) << endl;
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
