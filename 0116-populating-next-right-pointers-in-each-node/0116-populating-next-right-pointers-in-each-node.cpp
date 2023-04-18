04ced7a24bfe15c6fc791405da262c54eb2ba2af
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2023-04-18 22:55:22 +0530

    Time: 25 ms (29.60%), Space: 17.8 MB (8.65%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -16,61 +16,34 @@ public:
 };
 */
 
-// class Solution {
-// public:
-//     Node* connect(Node* root) {
-//         if(root == NULL) return NULL;
-//         queue<Node*> q;
-//         q.push(root);
-//         // int level= 0;
-        
-//         while(!q.empty()){
-//             int s = q.size();
-//             vector<Node*> level_nodes;
-//             for(int i=0; i<s;i++){
-//                 Node* node = q.front();
-//                 q.pop();
-//                 level_nodes.push_back(node);
-//                 if(node->left) q.push(node->left);
-//                 if(node->right) q.push(node->right);
-//             }
-//             // level ++;
-//             // for( auto it : level_nodes) cout<<it->val<<" ";
-//             // cout<<endl;
-//             // cout<<level<<endl;
-//             for(int i=0;i<level_nodes.size(); i++){
-//                 if(i == level_nodes.size()-1 ){
-//                     level_nodes[i]->next = NULL;
-//                 }
-//                 else
-//                 level_nodes[i]->next = level_nodes[i+1];
-//             }
-//         }
-//         return root;
-//     }
-// };
-
 class Solution {
 public:
     Node* connect(Node* root) {
+        if(root == NULL) return NULL;
         queue<Node*> q;
         q.push(root);
-
-        while(!q.empty()) {
-            int sz = q.size();
-            if(sz == 1 && q.front() == NULL) break;
-
-            for(int i = 0; i < sz; i++) {
-                auto top = q.front();
+        // int level= 0;
+        
+        while(!q.empty()){
+            int s = q.size();
+            vector<Node*> level_nodes;
+            for(int i=0; i<s;i++){
+                Node* node = q.front();
                 q.pop();
-                
-                if(i < sz - 1)
-                    top->next = q.front();
-
-                if(top->left) {
-                    q.push(top->left);
-                    q.push(top->right);
+                level_nodes.push_back(node);
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+            // level ++;
+            // for( auto it : level_nodes) cout<<it->val<<" ";
+            // cout<<endl;
+            // cout<<level<<endl;
+            for(int i=0;i<level_nodes.size(); i++){
+                if(i == level_nodes.size()-1 ){
+                    level_nodes[i]->next = NULL;
                 }
+                else
+                level_nodes[i]->next = level_nodes[i+1];
             }
         }
         return root;
