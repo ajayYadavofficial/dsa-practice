1aae169654f975cb0ffe21dd209515196797c86b
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2023-04-24 10:16:02 +0530

    Added solution - LeetHub

--- Distance from the Source (Bellman-Ford Algorithm) - GFG/distance-from-the-source-bellman-ford-algorithm.cpp
+++ Distance from the Source (Bellman-Ford Algorithm) - GFG/distance-from-the-source-bellman-ford-algorithm.cpp
@@ -14,37 +14,30 @@ class Solution {
     */
     vector<int> bellman_ford(int V, vector<vector<int>>& edges, int S) {
         // Code here
-        vector<int> dist (V, 1e8);
-        // vector<pair<int,int>> adj(V);
-        dist[S] = 0; 
         
-        for(int i=0;i<V-1;i++){
-        
-            for(auto it : edges){
+        vector<int> dist(V,1e8);
+        dist[S] = 0;
+        vector<int> dist2(V,1e8);
+        for(int i=0; i<V-1; i++){
+            for( auto it : edges){
                 int u = it[0];
                 int v = it[1];
-                int dis = it[2];
-                
-                if(dist[u] != 1e8 && dist[u] + dis < dist[v]){
-                    dist[v] = dist[u] + dis;
+                int wt= it[2];
+                if( dist[u] != 1e8 && dist[u] + wt < dist[v]){
+                    dist[v] = dist[u] + wt;
                 }
-                
             }
         }
         
-        for(auto it : edges){
+        for( auto it : edges){
                 int u = it[0];
                 int v = it[1];
-                int dis = it[2];
-                
-                if(dist[u] != 1e8 && dist[u] + dis < dist[v]){
+                int wt= it[2];
+                if( dist[u] != 1e8 && dist[u] + wt < dist[v]){
                     return {-1};
                 }
-                
             }
-        
-        return dist;
-        
+            return dist;
     }
 };
 
