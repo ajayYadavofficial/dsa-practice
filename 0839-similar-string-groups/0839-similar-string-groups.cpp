cd73ffd0d19a500f11f8d83af7932e170a351676
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2023-04-29 14:46:59 +0530

    Time: 203 ms (44.16%), Space: 107 MB (29.65%) - LeetHub

--- 0839-similar-string-groups/0839-similar-string-groups.cpp
+++ 0839-similar-string-groups/0839-similar-string-groups.cpp
@@ -1,46 +1,37 @@
 class Solution {
 public:
-    int p[300], r[300], sum;
-    int numSimilarGroups(vector<string>& strs) {
-        int n = strs.size();
-        sum = n;
-        for (int i = 0; i < n; i++) p[i] = i, r[i] = 1;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (similar(strs[i], strs[j])) {
-                    Union(i, j);
-                }
+    
+    void dfs(int i, vector<string>& strs, vector<bool> &visi){
+        visi[i] = true;
+        for(int j=0; j<strs.size(); j++){
+            if(visi[j]) continue;
+            if(is_similar(strs[i], strs[j]) ){
+                dfs(j,strs,visi);
             }
         }
-        return sum;
     }
     
-    bool similar(string const& s1, string const& s2) {
+    bool is_similar(string s1, string s2){
         int diff = 0;
-        for (int i = 0; i < s1.size(); i++) {
-            if(s1[i] != s2[i]) {
-                diff++;
-                if (diff > 2) return false;
-            }
+        for(int i=0; i<s1.size();i++){
+            if(s1[i] != s2[i]) diff++;
+            if(diff > 2) return false;
         }
-        return diff <= 2;
+        return (diff == 0 || diff == 2);
     }
     
-    void Union(int i, int j) {
-        int p1 = find(i);
-        int p2 = find(j);
-        if (p1 == p2) return;
-        if (r[p1] < r[p2]) swap(p1, p2);
-        p[p2] = p1;
-        r[p1] += r[p2];
-        sum--;
-    }
-    
-    int find(int i) {
-        while (i != p[i]) {
-            p[i] = p[p[i]];
-            i = p[i];
+    int numSimilarGroups(vector<string>& strs) {
+        
+        int groups = 0; 
+        int n = strs.size();
+        vector<bool> visi(n,0);
+        
+        
+        for(int i=0; i<n;i++){
+            if(visi[i]) continue;
+            groups++;
+            dfs(i,strs,visi);
         }
-        return i;
+        return groups;
     }
 };
