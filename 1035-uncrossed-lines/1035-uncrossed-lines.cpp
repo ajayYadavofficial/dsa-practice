1cf5956a591e0d8f104da9f4e35da1c9666af50c
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2023-05-11 18:09:03 +0530

    Time: 49 ms (11.00%), Space: 12.9 MB (63.96%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -7,9 +7,7 @@ public:
         if(v1[i]==v2[j]){
             ans=1+c(v1,v2,i+1,j+1,dp);
         }
-        ans=max({ans,c(v1,v2,i+1,j,dp),c(v1,v2,i,j+1,dp)});
-        dp[i][j]=ans;
-        return ans;
+        return dp[i][j] = max({ans,c(v1,v2,i+1,j,dp),c(v1,v2,i,j+1,dp)});
     }
     int maxUncrossedLines(vector<int>&v1, vector<int>& v2) {
         vector<vector<int>>dp(v1.size(),vector<int>(v2.size(),-1));
