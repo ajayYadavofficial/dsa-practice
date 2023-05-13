91fe79853e89876553cdea928367caaa2f21e398
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2023-05-13 12:32:50 +0530

    Time: 23 ms (27.22%), Space: 14.4 MB (37.54%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
@@ -1,50 +1,26 @@
-// class Solution {
-// public:
-//     int mod = 1e9+7;
-    
-//     int solve(int zero, int one, int len, int currLen,vector<int> &dp){
-//         if(currLen == len) return 1;
-//         if(currLen > len) return 0;
-//         if(dp[currLen] != -1) return dp[currLen];
-        
-//         int takeZero = solve(zero,one,len, currLen + zero,dp) % mod;
-            
-//         int takeOne = solve(zero,one,len,currLen + one,dp) % mod;
-        
-//         return dp[currLen] = (takeZero + takeOne) % mod;
-        
-//     }
-    
-//     int countGoodStrings(int low, int high, int zero, int one) {
-     
-//         int ans = 0;
-//         vector<int> dp (high +1, -1);
-//         for(int len = low; len <= high; len++){
-//             ans = (ans + solve(zero, one, len,0,dp) ) % mod;
-//         }
-//         return ans;
-//     }
-// };
-
-
 class Solution {
-    int mod=1e9+7;
-    int solve(int target,int one ,int zero,vector<int>&dp){
-        // good string 
-        if(target==0)return 1;
-        // not a goood string 
-        if(target<0)return 0;
-        if(dp[target]!=-1)return dp[target];
-        long long sum;
-        sum=solve(target-one,one,zero,dp)+solve(target-zero,one,zero,dp);
-        return dp[target]=sum%mod;
-    }
 public:
+    int mod = 1e9+7;
+    
+    int solve(int zero, int one, int len,vector<int> &dp){
+        if(len == 0) return 1;
+        if(len < 0 ) return 0;
+        if(dp[len] != -1) return dp[len];
+        
+        int takeZero = solve(zero,one,len- zero,dp) % mod;
+            
+        int takeOne = solve(zero,one,len-one,dp) % mod;
+        
+        return dp[len] = (takeZero + takeOne) % mod;
+        
+    }
+    
     int countGoodStrings(int low, int high, int zero, int one) {
-        int ans=0;
-        vector<int>dp(high+1,-1);
-        for(int i=low;i<=high;i++){
-            ans=((ans%mod)+solve(i,one,zero,dp))%mod;
+     
+        int ans = 0;
+        vector<int> dp (high +1, -1);
+        for(int len = low; len <= high; len++){
+            ans = (ans + solve(zero, one, len,dp) ) % mod;
         }
         return ans;
     }
