b53158d369efb760646869d205a74c5814416f3d
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2023-05-13 10:53:41 +0530

    Time: 395 ms (30.27%), Space: 115.2 MB (93.05%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -2,20 +2,28 @@
 class Solution {
 public:
     
-    ll solve(int ind,vector<vector<int>>& questions, vector<ll> &dp){
-        int n = questions.size();
-        if(ind >= n )return 0;
-        if(dp[ind] != -1) return dp[ind];
-        ll pick = -1e8, notPick = -1e8;
-        pick = questions[ind][0] + solve(ind + questions[ind][1]+1, questions, dp);
-        notPick = solve(ind+1, questions,dp);
-        return dp[ind] = max(pick,notPick);
+//     ll solve(int ind,vector<vector<int>>& questions, vector<ll> &dp){
+//         int n = questions.size();
+//         if(ind >= n)return 0;
+//         if(dp[ind] != -1) return dp[ind];
+//         ll pick = -1e8, notPick = -1e8;
+//         pick = questions[ind][0] + solve(ind + questions[ind][1]+1, questions, dp);
+//         notPick = solve(ind+1, questions,dp);
+//         return dp[ind] = max(pick,notPick);
         
-    }
+//     }
     
     long long mostPoints(vector<vector<int>>& questions) {
         int n = questions.size();
-        vector<ll> dp (n+1,-1);
-        return solve(0, questions, dp);
+        vector<ll> dp (n+1,0);
+        
+        for(int ind = n-1; ind>=0; ind--){
+            int nextQues = min(n,ind + questions[ind][1]+1);
+            ll pick = questions[ind][0] + dp[nextQues];
+            ll notPick = dp[ind+1];
+            dp[ind] = max(pick,notPick);
+        }
+        
+        return dp[0];
     }
 };
