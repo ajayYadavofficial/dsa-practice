cdd25640f090cdb2cfd358d19025eac0c09f4a43
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2023-05-17 12:44:03 +0530

    Time: 277 ms (38.29%), Space: 121.9 MB (50.29%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -26,34 +26,53 @@
 //     }
 // };
 
+// class Solution {
+//     public:
+//      int pairSum(ListNode* head) {
+//         ListNode* slow = head;
+//         ListNode* fast = head;
+//         int maxVal = 0;
+
+//         while(fast && fast -> next)
+//         {
+//             slow = slow -> next;
+//             fast = fast -> next -> next;
+//         }
+
+//         ListNode *nextNode, *prev = NULL;
+//         while (slow) {
+//             nextNode = slow->next;
+//             slow->next = prev;
+//             prev = slow;
+//             slow = nextNode;
+//         }
+
+//         while(prev)
+//         {
+//             maxVal = max(maxVal, head -> val + prev -> val);
+//             prev = prev -> next;
+//             head = head -> next;
+//         }
+
+//         return maxVal;
+//     }
+// };
+
 class Solution {
-    public:
-     int pairSum(ListNode* head) {
-        ListNode* slow = head;
-        ListNode* fast = head;
-        int maxVal = 0;
-
-        while(fast && fast -> next)
-        {
-            slow = slow -> next;
-            fast = fast -> next -> next;
+public:
+    int pairSum(ListNode* head) {
+        ListNode* temp=head;
+        stack<int>s;
+        while(temp){
+            s.push(temp->val);
+            temp=temp->next;
         }
-
-        ListNode *nextNode, *prev = NULL;
-        while (slow) {
-            nextNode = slow->next;
-            slow->next = prev;
-            prev = slow;
-            slow = nextNode;
-        }
-
-        while(prev)
-        {
-            maxVal = max(maxVal, head -> val + prev -> val);
-            prev = prev -> next;
-            head = head -> next;
-        }
-
-        return maxVal;
+       int mx=0;
+       while(s.size()){
+           mx=max(mx,s.top()+head->val);
+           head=head->next;
+           s.pop();
+       }
+       return mx;
     }
 };
