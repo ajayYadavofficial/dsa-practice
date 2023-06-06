b3827904580a2e3fed925eab9585230cf2a5a58e
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2023-06-06 08:40:20 +0530

    Time: 345 ms (45.07%), Space: 112.6 MB (88.07%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,36 +1,66 @@
+// class Solution {
+// public:
+//     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        
+//         unordered_map<int,vector<int>> manages;
+//         // <manager, employee>
+//         for(int i=0; i<n;i++){
+//             int mngr = manager[i];
+//             // employee is (I)
+//             if(mngr == -1) continue;
+//             manages[mngr].push_back(i);
+//         }
+        
+//         queue<pair<int,int>> q;
+//         // node,timeTillNow
+//         q.push({headID,0});
+//         int timeTaken = 0;
+//         while(!q.empty()){
+//             auto it = q.front();
+//             int head = it.first;
+//             int headTime = it.second;
+//             q.pop();
+//             if(manages[head].size() == 0) timeTaken = headTime;
+//                 for(auto subOrdinates : manages[head]){
+//                     q.push({subOrdinates, headTime+informTime[head]});
+//                 }
+            
+//         }
+        
+        
+//         // for (auto it : manages){
+//         //     cout<<it.first<<endl;
+//         //     for(auto it2 : it.second){
+//         //         cout<<it2<<" ";
+//         //     }
+//         //     cout<<endl;
+//         // }
+        
+        
+//         return timeTaken;
+//     }
+// };
+
+
 class Solution {
 public:
+    int dfs(int headId,vector<int>& informTime,vector<int> adj[]){
+
+        int maxTime = 0;
+
+        for(auto it:adj[headId])
+           maxTime = max(maxTime,dfs(it,informTime,adj));
+
+        return informTime[headId] + maxTime;   
+    }
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-        vector<int> arr[n];
-        int ans = 0;
+        
+            vector<int> adj[100005];
 
-        for(int i = 0; i < n; i++){
-            if(manager[i] != -1){
-                arr[manager[i]].push_back(i);
-            }
-        }
-
-        queue<pair<int, int>> q;
-        q.push({headID, informTime[headID]});
-
-        while(!q.empty()){
-            int si = q.size();
-
-            for(int i = 0; i < si; i++){
-                auto t = q.front();
-                q.pop();
-
-                for(auto x: arr[t.first]){
-                    if(informTime[x] == 0){
-                        ans = max(ans, t.second);
-                    }else{
-                        q.push({x, t.second + informTime[x]});
-                    }
-                }
-            }
-        }
-
-
-        return ans;
+            for(int i=0;i<manager.size();i++)
+                if(manager[i] != -1)
+                  adj[manager[i]].push_back(i);
+            
+        return dfs(headID,informTime,adj);    
     }
 };
