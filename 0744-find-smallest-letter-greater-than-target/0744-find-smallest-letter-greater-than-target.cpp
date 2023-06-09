b19409fb1290089e2b601604c25ef8057077d3e6
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2023-06-09 08:03:37 +0530

    Time: 26 ms (5.25%), Space: 15.9 MB (32.10%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -1,15 +1,34 @@
+// class Solution {
+// public:
+//     char nextGreatestLetter(vector<char>& letters, char target) {
+//         char ans = letters[0];
+//         for(auto it : letters){
+//             if(it  > target){
+//                 ans = it;
+//                 break;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        
-        char ans = letters[0];
-        sort(letters.begin(), letters.end());
-        for(auto it : letters){
-            if(it  > target){
-                ans = it;
-                break;
+        int left = 0;
+        int right = letters.size()-1;
+        int mid = 0;
+        if (letters[right] <= target) {
+            return letters[0];
+        }
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (letters[mid] > target) {
+                right = mid-1;
+            } else {
+                left = mid+1;
             }
         }
-        return ans;
+        return letters[left];
     }
 };
