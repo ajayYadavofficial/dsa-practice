ea84e94ce32f1e9c9a55daa9aa60ee34a3ba43cc
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2023-07-15 13:51:38 +0530

    Time: 415 ms (14.85%), Space: 120.5 MB (5.05%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -5,21 +5,20 @@ public:
     int solve( int ind,vector<vector<int>>& events, int k, map<pair<int,int>, int> &dp){
         
         if( k == 0 || ind >= events.size() ) return 0;
-        
-        
         // not take
         
         if(dp.find({ind,k}) != dp.end()) return dp[{ind,k}];
-        
-        
         int notTake = 0 + solve( ind+1, events, k, dp );
-        
         int i;
-        for(i = ind+1; i < events.size(); i++){
-            if( events[ind][1] < events[i][0] ) break;
-        }
-        int take = events[ind][2] + solve( i , events, k-1 ,dp);
+        // for(i = ind+1; i < events.size(); i++){
+        //     if( events[ind][1] < events[i][0] ) break;
+        // }
         
+        vector<int> temp = { events[ind][1], INT_MAX, INT_MAX };
+        
+        i = upper_bound(events.begin(), events.end(), temp) - events.begin();
+        
+        int take = events[ind][2] + solve( i , events, k-1 ,dp);
         return dp[{ind,k}] =  max(take, notTake);
         
     }
@@ -49,9 +48,7 @@ public:
     }*/
     
     int maxValue(vector<vector<int>>& events, int k) {
-        
         sort(events.begin(), events.end());
-        
         map<pair<int,int>, int> dp;
         
         // for(auto it : events){
