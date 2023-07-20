cf03646e7012a8b0d5f23f03201ba38b5c6e622b
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2023-07-20 08:43:39 +0530

    Time: 12 ms (83.98%), Space: 17.7 MB (76.60%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -68,47 +68,39 @@
 //     }
 // };
 
-
 class Solution {
 public:
-    vector<int> asteroidCollision(vector<int>& asteroids) {
-        list<int> stack{};
-
-        for (const int ast : asteroids)
-        {
-            bool check_stack = true;
-            while (check_stack)
-            {
-                if (stack.size() == 0)
-                {
-                    stack.emplace_back(ast);
-                    check_stack = false;
-                    continue;
-                }
-                bool collision = stack.back() > 0 && ast < 0; 
-                if (collision)
-                {
-                    if (abs(ast) == abs(stack.back()))
-                    {
-                        stack.pop_back();
-                        check_stack = false;
-                    }
-                    else if (abs(ast) > abs(stack.back()))
-                    {
-                        stack.pop_back();
-                    }
-                    else if (abs(ast) < abs(stack.back()))
-                    {
-                        check_stack = false;
-                    }
-                }
-                else
-                {
-                    stack.emplace_back(ast);
-                    check_stack = false;
+   bool samesign(int x,int y){
+       if(x<0&&y<0)
+       return true;
+       else if(x>0&&y>0)
+       return true;
+       return false;
+   }
+    vector<int> asteroidCollision(vector<int>& a) {
+        int n=a.size();
+        stack<int>st;
+          for(int i=0;i<n;i++){
+            if(st.size()==0 || (st.top()<0 && a[i]>0) || samesign(st.top(),a[i])){
+                st.push(a[i]); 
+            }else{
+                while(st.size()>0 && st.top()>0 && st.top() < abs(a[i])) st.pop();
+                
+                if(st.size()==0 || st.top()<0){
+                    st.push(a[i]);
+                }else if(st.top() == abs(a[i])){
+                    st.pop(); 
                 }
             }
         }
-        return vector<int>(stack.begin(), stack.end());
+        
+        vector<int> ans(st.size());
+        int i=st.size()-1;
+        while(!st.empty()){
+            ans[i]=st.top();
+            i--;
+            st.pop();
+        }
+        return ans;
     }
 };
