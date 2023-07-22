e0f3ce84f1ecd32e580b17ee4d15b468e6c8a75b
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2023-07-22 09:25:35 +0530

    Time: 13 ms (80.74%), Space: 6.6 MB (89.79%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
-
-double dp[30][30][105];
-int X[8]={ 2, 1, -1, -2, -2, -1, 1, 2 };
-int Y[8]={ 1, 2, 2, 1, -1, -2, -2, -1 };
-
-double dfs(int i,int j,int k, int n)
-{
-    if(i < 0 || j < 0 ||i >= n || j >= n) return 0;
-    if(k == 0) return 1;
-    if(dp[i][j][k] != 0) return dp[i][j][k];
-
-    double res = 0;
-
-    for(int l = 0;l < 8; ++l)
-    {
-      int x = i + X[l];
-      int y = j + Y[l];
-      res += dfs(x,y,k-1,n);
+    
+    double dp[26][26][101];
+    
+    double solve( int row, int col, int n, int k ){
+        
+        
+        
+        if(k == 0) return 1;
+        
+        if(dp[row][col][k] != 0) return dp[row][col][k];
+        
+        int drow[] = {-1,-2,-2,-1,+1,+2,+2,+1};
+        int dcol[] = {-2,-1,+1,+2,+2,+1,-1,-2};
+        double res = 0;
+        for(int i=0; i<8;i++){
+            
+            int nrow = row + drow[i];
+            int ncol = col + dcol[i];
+            
+            if( nrow < n && ncol < n && nrow >=0 && ncol >=0 )
+                res += (0.125)*solve(nrow,ncol,n,k-1);
+            
+        }
+        
+        return dp[row][col][k] = res;
+        
     }
-    return dp[i][j][k] += (res/8.0);
-}
-
+    
     double knightProbability(int n, int k, int row, int column) {
-
         memset(dp,0,sizeof(dp));
-        return dfs(row,column,k,n); 
+        return solve( row, column, n, k );
     }
 };
