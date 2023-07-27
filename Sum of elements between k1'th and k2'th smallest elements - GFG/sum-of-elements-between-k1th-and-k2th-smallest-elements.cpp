df3e6e9960b5f139ad4b753420fb96dd4262f973
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2023-07-27 23:23:55 +0530

    Added solution - LeetHub

--- Sum of elements between k1'th and k2'th smallest elements - GFG/sum-of-elements-between-k1th-and-k2th-smallest-elements.cpp
+++ Sum of elements between k1'th and k2'th smallest elements - GFG/sum-of-elements-between-k1th-and-k2th-smallest-elements.cpp
@@ -3,31 +3,35 @@
 using namespace std;
 
 // } Driver Code Ends
+
+#include<bits/stdc++.h>
+#define ll long long
+
 class Solution{
     public:
     long long sumBetweenTwoKth( long long A[], long long N, long long K1, long long K2)
     {
         // Your code goes here
-        priority_queue<long long> maxHeap;
         
-        for(long long i=0; i<N;i++){
-            maxHeap.push(A[i]);
+        priority_queue<ll> pq;
+        
+        for(ll i=0; i<N; i++){
+            pq.push(A[i]);
+            if(pq.size() > K2){
+                pq.pop();
+            }
+            
         }
-        
-        while( maxHeap.size() >= K2){
-            maxHeap.pop();
+        pq.pop();
+        ll cnt = K2-K1-1;
+        ll ans = 0;
+        while(cnt){
+            // cout<<"top element ="<<pq.top()<<endl;
+            ans += pq.top();
+            pq.pop();
+            cnt--;
         }
-        K2--;
-        // cout<<maxHeap.size()<<endl;
-        long long sum = 0;
-        for(long long i = 1; i<=K2-K1; i++ ){
-            sum += maxHeap.top();
-            // cout<<maxHeap.top()<<" maxHeapTop "<<i<<endl;
-            maxHeap.pop();
-        }
-        
-        return sum;
-        
+        return ans;
     }
 };
 
