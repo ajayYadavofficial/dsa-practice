50c02409604376d570ae60ed37737667d18f44bd
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2023-07-27 09:32:22 +0530

    Time: 348 ms (5.40%), Space: 191.9 MB (6.83%) - LeetHub

---
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
@@ -0,0 +1,44 @@
+#define ll long long
+
+class Solution {
+public:
+    
+    bool possible(ll x, int n, vector<int> batteries){
+        
+        ll sum =0;
+        
+        for(int i = 0; i<batteries.size(); i++){
+            
+            sum += batteries[i];
+            
+            if(sum >= x){
+                sum -= x;
+                n--;
+            }
+            if(n == 0) return 1;
+            
+        }
+        
+        return  0;
+        
+    }
+    
+    long long maxRunTime(int n, vector<int>& batteries) {
+        ll l = 0, r = LLONG_MAX, ans = 0;
+        sort(batteries.begin(), batteries.end());
+        while( l <= r){
+            // cout<<"l= "<<l<<" r= "<<r<<endl;
+            ll mid = (l+r)/2;
+            // cout<<"mid = "<<mid<<endl;
+            if(possible(mid, n,batteries )){
+                ans = mid;
+                l = mid+1;
+            }else{
+                r = mid-1;
+            }
+            
+        }
+        
+        return ans;
+    }
+};
