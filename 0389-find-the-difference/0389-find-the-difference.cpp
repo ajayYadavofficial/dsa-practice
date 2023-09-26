ef871f15a76b5e04f2b62631621ac0c3712718c6
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2023-09-26 11:31:08 +0530

    Time: 4 ms (47.89%), Space: 7.2 MB (5.22%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char c = 0;
+        
+        for(auto it:s){
+            c ^= it;
+        }
+        for(auto it: t){
+            c ^= it;
+        }
+        return c;
+    }
+};
