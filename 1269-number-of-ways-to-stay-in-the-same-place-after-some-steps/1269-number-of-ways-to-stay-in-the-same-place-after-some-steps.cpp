fa500c42760353f58d5ad842d4ea0408138bac1c
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2023-10-15 12:33:13 +0530

    Time: 31 ms (28.14%), Space: 13.9 MB (35.28%) - LeetHub

--- 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -1,3 +1,39 @@
+
+// #include <vector>
+// using namespace std;
+
+// class Solution {
+// public:
+//     int numWays(int steps, int arrLen) {
+//         int mod = 1000000007;
+//         int maxPosition = min(steps / 2, arrLen - 1);
+//         vector<vector<int>> memo(steps + 1, vector<int>(maxPosition + 1, -1));
+//         return waysToReturn(0, steps, maxPosition, memo, mod);
+//     }
+    
+//     int waysToReturn(int position, int steps, int maxPosition, vector<vector<int>>& memo, int mod) {
+//         if (position < 0 || position > maxPosition || position > steps / 2) {
+//             return 0;
+//         }
+//         if (steps == 0) {
+//             return (position == 0) ? 1 : 0;
+//         }
+//         if (memo[steps][position] != -1) {
+//             return memo[steps][position];
+//         }
+        
+//         int ways = 0;
+//         ways = (ways + waysToReturn(position, steps - 1, maxPosition, memo, mod)) % mod;
+//         ways = (ways + waysToReturn(position - 1, steps - 1, maxPosition, memo, mod)) % mod;
+//         ways = (ways + waysToReturn(position + 1, steps - 1, maxPosition, memo, mod)) % mod;
+        
+//         memo[steps][position] = ways;
+//         return ways;
+//     }
+// };
+
+
+
 #include <vector>
 using namespace std;
 
