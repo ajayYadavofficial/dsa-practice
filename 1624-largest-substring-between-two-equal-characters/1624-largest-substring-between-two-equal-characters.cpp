f7dc023345b824a4c9198959d8d5ef141d0c3bfb
Ajay Yadav <101542376+ajayYadavofficial@users.noreply.github.com>
2023-12-31 12:16:57 +0530

    Time: 5 ms (5.74%), Space: 6.7 MB (41.15%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.cpp
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.cpp
@@ -1,17 +1,30 @@
+// class Solution {
+// public:
+//     int maxLengthBetweenEqualCharacters(string s) {
+//         map<char, vector<int>> mp;
+//         int maxi = INT_MIN;
+//         for(int i = 0; i< s.size(); i++){
+//             mp[s[i]].push_back(i);
+//             maxi = max( mp[s[i]][mp[s[i]].size()-1]- mp[s[i]][0], maxi  );
+//         }
+        
+//         if(maxi == 0) return -1;
+//         return maxi-1;
+//     }
+// };
+
 class Solution {
 public:
     int maxLengthBetweenEqualCharacters(string s) {
-        map<char, vector<int>> mp;
-        int maxi = INT_MIN;
-        for(int i = 0; i< s.size(); i++){
-            
-            mp[s[i]].push_back(i);
-            // cout<<mp[s[i]][0];
-            maxi = max( mp[s[i]][mp[s[i]].size()-1]- mp[s[i]][0], maxi  );
-            
+        int ans = -1;
+        for (int left = 0; left < s.size(); left++) {
+            for (int right = left + 1; right < s.size(); right++) {
+                if (s[left] == s[right]) {
+                    ans = max(ans, right - left - 1);
+                }
+            }
         }
         
-        if(maxi == 0) return -1;
-        return maxi-1;
+        return ans;
     }
 };
